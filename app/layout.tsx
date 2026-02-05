40dcd4d8f2d60d4c1b35c203099e387f17e7f885
emanuelgimenez2 <emanuelgimenez2@gmail.com>
2026-02-05 09:33:00 -0300

    metadatos y fuera formulario inicial

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,32 @@ const playfair = Playfair_Display({
 })
 
 export const metadata = {
-  title: "El Mangrullo - Cabañas",
+  title: "El Mangrullo | Departamentos | Federación Entre Rios",
   description: "El Mangrullo - Su hogar lejos de casa",
-  generator: 'v0.dev'
+  metadataBase: new URL("https://mangrullo.vercel.app/"),
+  openGraph: {
+    title: "El Mangrullo | Departamentos | Federación Entre Rios",
+    description: "El Mangrullo - Su hogar lejos de casa",
+    url: "https://mangrullo.vercel.app/",
+    siteName: "El Mangrullo",
+    images: [
+      {
+        url: "/metadatos.png",
+        width: 1600,
+        height: 837,
+        alt: "El Mangrullo - Departamentos",
+      },
+    ],
+    locale: "es_AR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "El Mangrullo | Departamentos | Federación Entre Rios",
+    description: "El Mangrullo - Su hogar lejos de casa",
+    images: ["/metadatos.png"],
+  },
+  generator: "v0.dev",
 }
 
 export default function RootLayout({
